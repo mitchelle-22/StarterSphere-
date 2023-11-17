322e6fd0707c8c4448bb6cbb1bd25eb74ba19d3b
Mich <mitchellefetty@gmail.com>
2023-11-17 13:20:02 +0200

    Model created

---
+++ StarterSphere/Starter.swift
@@ -0,0 +1,38 @@
+//
+//  Starter.swift
+//  StarterSphere
+//
+//  Created by MIch on 2023/11/17.
+//
+
+
+import SwiftUI
+
+struct Starter: Decodable{
+    
+    let id : Int
+    let name : String
+    let description : String
+    let price : Double
+    let imageUrl : String
+    let calories : Int
+    let protein : Int
+    let carbs : Int
+}
+
+struct StaterRespose{
+    let request:[Starter]
+}
+
+struct MockData{
+    static let sampleStarter = Starter(id: 01,
+                                      name:"Test Staterter",
+                                      description: "",
+                                      price: 100.00 ,
+                                      imageUrl: "",
+                                      calories: 12,
+                                      protein: 20,
+                                      carbs: 11)
+    static let starters = [sampleStarter,sampleStarter,sampleStarter,sampleStarter]
+}
+ 
