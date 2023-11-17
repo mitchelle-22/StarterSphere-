974e02ff72782af25dff46e1521de52429ddb30e
Mich <mitchellefetty@gmail.com>
2023-11-17 11:36:42 +0200

    Initial Commit

---
+++ StarterSphere/StarterSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterSphereApp.swift
+//  StarterSphere
+//
+//  Created by MIch on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct StarterSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
